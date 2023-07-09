5ba288b1a613e039c55c764f225efcb3c8cb804f
Matheus Valentim <meduardo.valentim@gmail.com>
2023-07-09 12:26:17 -0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Header } from "./components/Header";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
-import { Router } from "./Router";
-import { BrowserRouter } from "react-router-dom";
 
 export function App() {
   return (
