dc7e100e4797a7a58116dfd5914c95e14d8d0e77
Matheus <meduardo.valentim@gmail.com>
2024-05-29 14:34:18 -0300

    .

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir arquivos estáticos da pasta 'build' (ajuste conforme necessário)
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Redirecionar todas as requisições para 'index.html'
-app.get('*', (req, res) => {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(3000);
